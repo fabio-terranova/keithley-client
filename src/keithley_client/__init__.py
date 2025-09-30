171bc91906c24611b9164b55f74497158f44f665
fabio-terranova <fabio.terranova@outlook.it>
2025-09-30 17:06:58 +0200

    chore: bump version to 0.6.0

--- src/keithley_client/__init__.py
+++ src/keithley_client/__init__.py
@@ -17,7 +17,7 @@ from PyQt5.QtWidgets import QApplication
 from .config import CONFIGS
 from .gui.MainWindow import MainWindow
 
-__version__ = "0.5.2"
+__version__ = "0.6.0"
 __author__ = "Fabio T"
 __all__ = ["PyQt5"]
 
